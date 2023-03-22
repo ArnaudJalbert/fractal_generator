efbc81fcdd06c2c691eda53490b787d736d2d862
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2023-03-21 21:33:11 -0400

    computing the normal

--- fragmentShader.glsl
+++ fragmentShader.glsl
@@ -1,9 +1,10 @@
 #version 330 core
 
-#define MAX_RAYMARCH_STEPS 64
-#define EPSILON 0.001f
+#define MAX_RAYMARCH_STEPS 256
+#define EPSILON 0.00001f
 #define MAX_DISTANCE 150
 #define INFINITY 999999999
+#define NORMAL_OFFSET vec2(EPSILON, 0)
 
 smooth in vec3 fragPosition; // position of the fragment
 out vec4 outColor;
@@ -46,10 +47,7 @@ From a ray origin and direction, it checks if that ray intersects with a geometr
 ro: ray origin -> vec3
 rd: ray direction -> vec3
 */
-bool raymarching(in vec3 ro, in vec3 rd){
-
-    // current raymarching point
-    vec3 p;
+bool raymarching(in vec3 ro, in vec3 rd, out vec3 p){
 
     // total distance traveled by ray
     float dt = 0;
@@ -62,7 +60,7 @@ bool raymarching(in vec3 ro, in vec3 rd){
         }
 
         // compute the current raymarching position
-        vec3 p = ro + rd * dt;
+        p = ro + rd * dt;
 
         // compute the closest point to the current raymarching position
         float d = map(p);
@@ -80,6 +78,21 @@ bool raymarching(in vec3 ro, in vec3 rd){
 }
 
 /*
+Computes the normal of a point using forward and central differences
+See this article for more details about this technique:
+p: point -> vec3
+*/
+vec3 normal(in vec3 p){
+
+    vec3 normal = vec3(map(p+NORMAL_OFFSET.xyy) - map(p-NORMAL_OFFSET.xyy),
+                  map(p+NORMAL_OFFSET.yxy) - map(p-NORMAL_OFFSET.yxy),
+                  map(p+NORMAL_OFFSET.yyx) - map(p-NORMAL_OFFSET.yyx)
+                  );
+
+    return normalize(normal);
+}
+
+/*
 From uv and the set geometry, it renders the color of the fragment
 uv: xy position of the current
 */
@@ -92,10 +105,17 @@ void render(in vec3 fragPosition, out vec3 color){
                         cameraRight * fragPosition.x * aspectRatio +
                         cameraUp * fragPosition.y);
 
+    vec3 p; // point of intersection
+
+    bool intersection = raymarching(ro, rd, p);
+
     // check if there is an intersection
-    if(raymarching(ro, rd)){
+    if(intersection){
         // TODO calculate the shading with the information
-        color = vec3(rd);
+        // computing the normal of the point
+        vec3 normal = normal(p);
+
+        color = vec3(normal);
     }
     else{
         // TODO define default BG color
