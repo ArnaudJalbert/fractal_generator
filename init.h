ab7fcf613cea5a221815eea75adfeee9fa58509f
ArnaudJalbert <74669399+ArnaudJalbert@users.noreply.github.com>
2023-03-21 20:56:45 -0400

    camera is set up ray origin/direction is set up raymarching loop is set up sphere intersection is set up

--- init.h
+++ init.h
@@ -12,6 +12,8 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 using namespace std;
 using namespace glm;
--- vertexShader.glsl
+++ vertexShader.glsl
@@ -1,8 +1,11 @@
 #version 330 core
 
-layout (location = 0) in vec3 pos; // the position variable has attribute position 0
+in vec3 position;
+out vec3 fragPosition;
 
 void main()
 {
-    gl_Position = vec4(pos, 1.0);
+    fragPosition = position;
+    gl_Position = vec4(position, 1.0f);
+
 }
